d80eef259db9a55eb5714f3151ae6d9370dfa4e7
vinozganic <jakov.vinozganic@fer.hr>
2024-11-10 01:22:23 +0100

    Update database connection url

--- weak-webapp/src/db.ts
+++ weak-webapp/src/db.ts
@@ -6,11 +6,12 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT ?? '5436'),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    // host: process.env.DATABASE_HOST,
+    // port: parseInt(process.env.DATABASE_PORT ?? '5436'),
+    // username: process.env.DATABASE_USERNAME,
+    // password: process.env.DATABASE_PASSWORD,
+    // database: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL,
     synchronize: true,
     logging: false,
     ssl: true,
